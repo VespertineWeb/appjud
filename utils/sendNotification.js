951b90e6df5a8d6f256558758fdec0ebaf661846
Christian <christian.fernds@gmail.com>
2024-07-18 13:39:57 -0300

    teste send pulse

--- utils/sendNotification.js
+++ utils/sendNotification.js
@@ -1,31 +1,4 @@
-import sendpulse from 'sendpulse-api';
-
-const SENDPULSE_USER_ID = process.env.SENDPULSE_USER_ID;
-const SENDPULSE_SECRET = process.env.SENDPULSE_SECRET;
-
-let tokenStorage = '';
-
-const tokenStorageFunc = {
-  save_token: (data) => {
-    tokenStorage = data;
-  },
-  get_token: () => {
-    return tokenStorage;
-  }
-};
-
-sendpulse.init(SENDPULSE_USER_ID, SENDPULSE_SECRET, tokenStorageFunc, () => {
-  console.log('SendPulse initialized');
-});
-
+// utils/sendNotification.js
 export const sendWhatsAppNotification = (phone, message) => {
-  const data = {
-    "phone": phone,
-    "message": message,
-    "sender": "YourSenderID",
-  };
-
-  sendpulse.whatsappSendMessage(data, (response) => {
-    console.log(response);
-  });
+  console.log(`Mock notification sent to ${phone}: ${message}`);
 };
