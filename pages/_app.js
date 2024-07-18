63546ddf6ca79599a08bf3dece21c8e69b4f3748
Christian <christian.fernds@gmail.com>
2024-07-17 23:55:50 -0300

    Configura LogRocket com ID de aplicação correto

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,9 @@
-// Diretório: `pages`
-// Arquivo: `_app.js`
-
 import LogRocket from 'logrocket';
 import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    LogRocket.init('wwhvxc/appjud');
+    LogRocket.init('wwhvxc/appjud'); // Substitua pelo seu ID de aplicação correto
   }, []);
 
   return <Component {...pageProps} />;
