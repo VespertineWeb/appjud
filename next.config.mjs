e5a886f1ee8450b98087eaf0a0fad46b9f2f25ce
Christian <christian.fernds@gmail.com>
2024-07-17 23:29:35 -0300

    Fix next.config.mjs syntax

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,8 @@
 export default {
   env: {
     MONGODB_URI: process.env.MONGODB_URI,
+    SENDPULSE_USER_ID: process.env.SENDPULSE_USER_ID,
+    SENDPULSE_SECRET: process.env.SENDPULSE_SECRET,
+    SENDPULSE_TOKEN_STORAGE: process.env.SENDPULSE_TOKEN_STORAGE,
   },
 };
