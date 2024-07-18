99b42e56c966c0e8047da2716e9a90db409b28a7
Christian <christian.fernds@gmail.com>
2024-07-17 23:49:55 -0300

    Remove Sentry integration

--- sentry.client.config.js
+++
@@ -1,6 +0,0 @@
-// sentry.client.config.js
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: 'your-sentry-dsn',
-});
--- sentry.server.config.js
+++
@@ -1,6 +0,0 @@
-// sentry.server.config.js
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: 'your-sentry-dsn',
-});
