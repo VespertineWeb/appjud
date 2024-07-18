b701e9d29cbb7dea12d3b4fe00f11c81938c3b96
Christian <christian.fernds@gmail.com>
2024-07-18 02:03:06 -0300

    aaaa22

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,34 +1,26 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+const connection = {};
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
+  if (connection.isConnected) {
+    return;
   }
 
-  if (!cached.promise) {
-    const opts = {
+  // Adicionar logs para verificar a URL do MongoDB
+  console.log('MongoDB URI:', process.env.MONGODB_URI);
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
+      useUnifiedTopology: true
     });
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log('Database connected:', connection.isConnected);
+  } catch (error) {
+    console.error('Database connection error:', error);
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
 }
 
 export default dbConnect;
